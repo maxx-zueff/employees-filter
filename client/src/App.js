2e7851dbf205da6d2deb77898209e52a068a3627
Max Zueff <maxx.zueff@gmail.com>
2020-12-03 20:49:03 +0300

    filter

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortAmountDownAlt, faSortAmountUp } from '@fortawesome/free-solid-svg-icons'
 
 import React from "react";
 import axios from "axios";
+import Table from 'react-bootstrap/Table'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
-const callAPI = function (url, method = "get", data = {}) {
+
+export const callAPI = function (url, method = "get", data = {}) {
   return axios({
     method: method,
     url: url,
@@ -12,54 +18,147 @@ const callAPI = function (url, method = "get", data = {}) {
   });
 };
 
-const filterByRole = function (docs, param) {
-  return docs.filter((doc) => doc.role == param);
+const filterByRole = function (employees, param) {
+  if (param == "*" ) return employees;
+  return employees.filter((doc) => doc.role == param);
 };
 
-const filterByArchive = function (docs, param) {
-  return docs.filter((doc) => doc.isArchive == param);
+const filterByArchive = function (employees, param) {
+  return employees.filter((doc) => doc.isArchive == param);
 };
+
+const getRoleName = function (role) {
+  let name = {
+    "driver" : "Курьер",
+    "waiter" : "Официант",
+    "cook" : "Повар",
+    "*" : "Все должности"
+  }
+
+  return name[role];
+}
+
+export const sorter = {
+
+  byName : function (employees) {
+     return employees.sort(function (a, b) {
+      let first = a.name
+      let second = b.name
+
+      if (first > second) return 1
+      else if (second > first) return -1
+      return 0
+    })
+  },
+
+  byBirthday : function (employees) {
+    return employees.sort(function (a, b) {
+      let first = new Date(a.joinedDate)
+      let second = new Date(b.joinedDate)
+      return first - second;
+    })
+  },
+
+  byNameReverce : function (employees) {
+    return sorter.byName(employees).reverse()
+  },
+
+  byBirthdayReverce : function (employees) {
+    return sorter.byBirthday(employees).reverse()
+  }
+}
+
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      default: [],
       employees: [],
+      roles: [],
+      selectedRole: "Все должности"
     };
-    this.filterByDriver = this.filterByDriver.bind(this);
+    this.filterByRole = this.filterByRole.bind(this);
   }
 
   componentDidMount() {
     callAPI("/find-every-employee")
-      .then((body) => this.setState({ employees: body.data }))
+      .then((body) => {
+        let roles = [];
+        let uniqueRole = []
+
+        body.data.map((employee) => {
+          if (employee.role !== undefined) roles.push(employee.role);
+          uniqueRole = [...new Set(roles)];
+        })
+
+        this.setState({
+          employees: body.data,
+          default: body.data,
+          roles: uniqueRole
+        })
+      })
       .catch((err) => console.log(err));
   }
 
-  filterByDriver() {
-    let drivers = filterByRole(this.state.employees, "driver");
-    this.setState({ employees: drivers });
+  filterByRole(role) {
+    let roles = filterByRole(this.state.default, role);
+
+    this.setState({
+      default: this.state.default,
+      roles: this.state.roles,
+      employees: roles,
+      selectedRole: getRoleName(role)
+    });
   }
+
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.employees.map((employee) => (
-              <li>{employee.name}</li>
+        <Navbar>
+          <Navbar.Brand href="#home">EmployeesTable</Navbar.Brand>
+          <NavDropdown title={this.state.selectedRole} id="collasible-nav-dropdown">
+            {this.state.roles.map((role) => (
+              <NavDropdown.Item onClick={(e) => this.filterByRole(role)}>{getRoleName(role)}</NavDropdown.Item>
             ))}
-          </ul>
-          <button onClick={this.filterByDriver}>Activate Lasers</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Изучай React
-          </a>
-        </header>
+            <NavDropdown.Divider />
+            <NavDropdown.Item onClick={(e) => this.filterByRole("*")}>Все должности</NavDropdown.Item>
+          </NavDropdown>
+        </Navbar>
+
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>
+                <span style={{paddingRight:5+'px'}}>Имя</span>
+                <FontAwesomeIcon icon={faSortAmountDownAlt}></FontAwesomeIcon>
+              </th>
+              <th>
+                <span style={{paddingRight:5+'px'}}>Должность</span>
+                {/* <FontAwesomeIcon icon={faSortAmountDownAlt}></FontAwesomeIcon> */}
+                <FontAwesomeIcon icon={faSortAmountUp}></FontAwesomeIcon>
+              </th>
+              <th>Телефон</th>
+              <th>Дата рождения</th>
+              <th>В архиве</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map((employee, index) => (
+              <tr>
+                <td>{index}</td>
+                <td>{employee.name}</td>
+                <td>{getRoleName(employee.role)}</td>
+                <td>{employee.phone}</td>
+                <td>{employee.birthday}</td>
+                <td>{(employee.isArchive) ? "+" : ""}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
--- curl.sh
+++ curl.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # curl -X POST -H "Content-Type: application/json" \
-#     -d '{"id": "5fc57cfac32b320fb0273ab7","name": "blabla"}' \
-#     localhost:5000/api/update-employee
+#     -d '{"id": "5fc57cfac32b320fb0273ab7","isArchive": true}' \
+#     localhost:5000/update-employee
 
 # curl -X POST -H "Content-Type: application/json" \
 #     -d '{"role": "waiter"}' \
-#     localhost:5000/api/filter-role-employee
+#     localhost:5000/filter-role-employee
 
 # curl -X POST -H "Content-Type: application/json" \
 #     -d '{"isArchive": false}' \
-#     localhost:5000/api/filter-archive-employee
+#     localhost:5000/filter-archive-employee
 
 # curl -X POST -H "Content-Type: application/json" \
 #     -d '{"name":"lolol","role":"driver","phone":"9056346690","birthday":"05011996","isArchive":"false"}' \
-#     localhost:5000/api/add-employee
+#     localhost:5000/add-employee
 
 # curl -X GET \
-#     localhost:5000/api/find-every-employee
+#     localhost:5000/find-every-employee
 #     
